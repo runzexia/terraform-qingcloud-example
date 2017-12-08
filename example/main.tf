d0acd9c5d83e07052328edd8625bb0ab8d6b76d3
runzexia <root@junwuhui.cn>
2017-12-08 13:25:49 +0800

    reformat code

--- example/main.tf
+++ example/main.tf
@@ -13,11 +13,13 @@ resource "qingcloud_instance" "foo" {
 }
 
 resource "null_resource" "run_docker_nginx" {
-  depends_on = [ "qingcloud_vpc_static.ssh-portforward" ,
-              "qingcloud_vpc_static.http-portforward",
-              "qingcloud_security_group_rule.ssh-in",
-              "qingcloud_security_group_rule.http-in",
-              "qingcloud_instance.foo" ]
+  depends_on = ["qingcloud_vpc_static.ssh-portforward",
+    "qingcloud_vpc_static.http-portforward",
+    "qingcloud_security_group_rule.ssh-in",
+    "qingcloud_security_group_rule.http-in",
+    "qingcloud_instance.foo",
+  ]
+
   provisioner "remote-exec" {
     inline = [
       "curl -fsSL get.docker.com -o get-docker.sh",
@@ -25,10 +27,11 @@ resource "null_resource" "run_docker_nginx" {
       "systemctl start docker",
       "docker run --name docker-nginx -d -p 80:80 nginx",
     ]
+
     connection {
-      type = "ssh"
-      user = "root"
-      host = "${qingcloud_vpc.foo.public_ip}"
+      type        = "ssh"
+      user        = "root"
+      host        = "${qingcloud_vpc.foo.public_ip}"
       private_key = "${file("~/.ssh/id_rsa")}"
     }
   }
@@ -41,7 +44,7 @@ resource "qingcloud_security_group" "foo" {
 resource "qingcloud_vpc" "foo" {
   security_group_id = "${qingcloud_security_group.foo.id}"
   vpc_network       = "192.168.0.0/16"
-  eip_id = "${qingcloud_eip.foo.id}"
+  eip_id            = "${qingcloud_eip.foo.id}"
 }
 
 resource "qingcloud_vxnet" "foo" {
@@ -50,37 +53,38 @@ resource "qingcloud_vxnet" "foo" {
   ip_network = "192.168.0.0/24"
 }
 
-resource "qingcloud_vpc_static" "http-portforward"{
+resource "qingcloud_vpc_static" "http-portforward" {
   vpc_id = "${qingcloud_vpc.foo.id}"
-  type = 1
-  val1 = "80"
-  val2 = "${qingcloud_instance.foo.private_ip}"
-  val3 = "80"
-}
-resource "qingcloud_vpc_static" "ssh-portforward"{
-  vpc_id = "${qingcloud_vpc.foo.id}"
-  type = 1
-  val1 = "22"
-  val2 = "${qingcloud_instance.foo.private_ip}"
-  val3 = "22"
+  type   = 1
+  val1   = "80"
+  val2   = "${qingcloud_instance.foo.private_ip}"
+  val3   = "80"
 }
 
-resource "qingcloud_security_group_rule" "http-in"{
-  security_group_id= "${qingcloud_security_group.foo.id}"
-  protocol = "tcp"
-  priority = 0
-  action = "accept"
-  direction = 0
-  from_port = 80
-  to_port = 80
-}
-resource "qingcloud_security_group_rule" "ssh-in"{
-  security_group_id= "${qingcloud_security_group.foo.id}"
-  protocol = "tcp"
-  priority = 0
-  action = "accept"
-  direction = 0
-  from_port = 22
-  to_port = 22
+resource "qingcloud_vpc_static" "ssh-portforward" {
+  vpc_id = "${qingcloud_vpc.foo.id}"
+  type   = 1
+  val1   = "22"
+  val2   = "${qingcloud_instance.foo.private_ip}"
+  val3   = "22"
 }
 
+resource "qingcloud_security_group_rule" "http-in" {
+  security_group_id = "${qingcloud_security_group.foo.id}"
+  protocol          = "tcp"
+  priority          = 0
+  action            = "accept"
+  direction         = 0
+  from_port         = 80
+  to_port           = 80
+}
+
+resource "qingcloud_security_group_rule" "ssh-in" {
+  security_group_id = "${qingcloud_security_group.foo.id}"
+  protocol          = "tcp"
+  priority          = 0
+  action            = "accept"
+  direction         = 0
+  from_port         = 22
+  to_port           = 22
+}
